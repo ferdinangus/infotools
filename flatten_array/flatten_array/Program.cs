366a63f9206672363bbd031812437c92b5c9ab3a
ferdinangus <folivera.acevedo@gmail.com>
2016-03-08 11:05:18 +1300

    upload test projects

---
+++ flatten_array/flatten_array/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flatten_array
+{
+    class Program
+    {
+        public static List<object> list = new List<object>();
+
+        static void Main(string[] args)
+        {
+            int[][] twoDimensional = new int[][] { 
+                                      new int[] {1, 2},
+                                      new int[] {3, 4},
+                                      new int[] {5, 6}
+                                     };
+
+            int[][][] threeDimensional = new int[][][] { 
+                                      new int[][] 
+                                      {
+                                          new int[] {1, 2},
+                                          new int[] {3, 4}
+                                      },
+                                      new int[][] 
+                                      {
+                                          new int[] {5, 6},
+                                          new int[] {7, 8}
+                                      },
+                                      new int[][] 
+                                      {
+                                          new int[] {9, 10},
+                                          new int[] {11, 12}
+                                      },
+                                     };
+
+            var result = flatten(threeDimensional);
+
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static object[] flatten(object[] array)
+        {
+            foreach (var item in array)
+            {
+                Type type = item.GetType();
+                if (type.IsArray && type != typeof(Int32[]))
+                {
+                    array = flatten((object[])item);
+                }
+                else if (type.IsArray && type == typeof(Int32[]))
+                {
+                    foreach (var item2 in (Int32[])item)
+                    {
+                        list.Add(item2);
+                    }
+                }
+                else
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ html_checker/html_checker/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace html_checker
+{
+    class Program
+    {
+        public static List<string> tags = new List<string>();
+        public static List<string> queue = new List<string>();
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                bool message_displayed = false;
+                Console.WriteLine("Press A for enter an HTML");
+                Console.WriteLine("Press E for exit");
+
+                char c = Convert.ToChar(Console.ReadLine());                
+
+                switch (c) {
+                    case 'A':
+                    case 'a':
+                        Console.WriteLine("Please enter an HTML text to check if it's written propertly");
+                        string input = Console.ReadLine();
+                        string val = get_html_tags(input);
+
+                        for (int i = 0; i < tags.Count; i++)
+                        {
+                            if (tags[i].Contains("/>"))
+                            {
+                                tags.Remove(tags[i]);
+                                i--;
+                            }
+                            else if (tags[i].Contains("<<d>")) 
+                            {
+                                tags.Remove(tags[i]);
+                                i--;
+                            }
+                            else if (!tags[i].Contains("</"))
+                            {
+                                queue.Add(tags[i]);
+                                tags.Remove(tags[i]);
+                                i--;
+                            }
+                            else if (queue.Count > 0 && queue.Last().Split('<', '>')[1].Contains(tags[i].Split('/', '>')[1]))
+                            {
+                                queue.Remove(queue.Last());
+                                tags.Remove(tags[i]);
+                                i--;
+                            }
+                            else if (queue.Count > 0 && !queue.Last().Split('<', '>')[1].Contains(tags[i].Split('/', '>')[1]))
+                            {
+                                Console.WriteLine("Missing " + queue.Last() + "c lose tag instead of " + tags[i]);
+                                message_displayed = true;
+                            }
+                        }
+
+                        if (tags.Count == 0 && queue.Count == 0 && !message_displayed)
+                        {
+                            Console.WriteLine("Correctly tagged paragraph");
+                        }
+                        else if (queue.Count > 0 && !message_displayed)
+                        {
+                            Console.Write("Missing");
+
+                            foreach (var item in queue)
+                            {
+                                Console.Write(" " + item + " ");
+                            }
+
+                            Console.WriteLine("close tag");
+                        }
+                        else if (tags.Count > 0 && !message_displayed)
+                        {
+                            Console.Write("Missing");
+
+                            foreach (var item in tags)
+                            {
+                                Console.Write(" " + item + " ");
+                            }
+
+                            Console.WriteLine("open tag");
+                        }
+
+                        break;
+                    case 'E':
+                    case 'e':
+                        return;
+                }                
+            }
+        }
+
+        private static string get_html_tags(string input)
+        {
+            string regex = @"<"
+                    + @"(?<endTag>/)?"    //Captures the / if this is an end tag.
+                    + @"(?<tagname>\w+)"    //Captures TagName
+                    + @"("                //Groups tag contents
+                    + @"(\s+"            //Groups attributes
+                    + @"(?<attName>\w+)"  //Attribute name
+                    + @"("                //groups =value portion.
+                    + @"\s*=\s*"            // = 
+                    + @"(?:"        //Groups attribute "value" portion.
+                    + @"""(?<attVal>[^""]*)"""    // attVal='double quoted'
+                    + @"|'(?<attVal>[^']*)'"        // attVal='single quoted'
+                    + @"|(?<attVal>[^'"">\s]+)"    // attVal=urlnospaces
+                    + @")"
+                    + @")?"        //end optional att value portion.
+                    + @")+\s*"        //One or more attribute pairs
+                    + @"|\s*"            //Some white space.
+                    + @")"
+                    + @"(?<completeTag>/)?>";
+
+            string text = String.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals('<'))
+                {
+                    text = input.Substring(i, Math.Max(input.IndexOf('>') + 1, 0) - i);
+                    
+                    int lastLocation = input.IndexOf( ">" );
+                    if(lastLocation >= 0)
+                    {
+                        input =  input.Substring( lastLocation + 1 );
+
+                        if (System.Text.RegularExpressions.Regex.IsMatch(text, regex))
+                        {
+                            tags.Add(text);                            
+                        }
+
+                        input = get_html_tags(input);
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
+}
